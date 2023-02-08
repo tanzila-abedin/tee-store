4212e079590c4d7e1162796cd0d4b3eae0402292
Tanzila Abedin <tanzila.abedin@yahoo.com>
2023-02-08 15:24:22 +0200

    Add increment, decrement  and remove function to cart Items

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,5 +1,7 @@
 import { cartActions } from "../store/cartSlice";
 import { useDispatch } from "react-redux";
+import { RiDeleteBin6Line } from "react-icons/ri";
+
 const CartItem = ({ details }) => {
   const { product, quantity } = details;
   const dispatch = useDispatch();
@@ -12,17 +14,35 @@ const CartItem = ({ details }) => {
     dispatch(cartActions.decrement(product.id));
   };
 
+  const onRemoveQuantity = () => {
+    dispatch(cartActions.removeProduct(product.id));
+  };
+
   console.log(product);
   return (
     <>
-      <div className="grid grid-cols-3 text-center my-2">
-        <p>{product.title}</p>
-        <p>R {product.price}</p>
-        <div className="">
-          <button onClick={onIncrementQuantity}> ADD</button>
-          <p>{quantity}</p>
-          <button onClick={onDecrementQuantity}>SUBTRACT</button>
+      <div className="grid grid-cols-4 text-center py-10">
+        <p className=" text-teal-400 font-medium text-lg">{product.title}</p>
+        <p className="text-lg">R {product.price}</p>
+        <div className="grid grid-cols-3 gap-0 m-0">
+          <button
+            onClick={onDecrementQuantity}
+            className="bg-gray-300 rounded-xl text-4xl text-white"
+          >
+            -
+          </button>
+          <p className="text-lg">{quantity}</p>
+          <button
+            onClick={onIncrementQuantity}
+            className=" bg-teal-400  rounded-xl text-4xl text-white"
+          >
+            {" "}
+            +
+          </button>
         </div>
+        <button onClick={onRemoveQuantity} className="text-center">
+          <RiDeleteBin6Line color="red" className="h-10 w-10 ml-48" />
+        </button>
       </div>
     </>
   );
