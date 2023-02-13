8b96faf7b584aa513056298c5f9a2c9f8dcb51bf
Tanzila Abedin <tanzila.abedin@yahoo.com>
2023-02-13 12:08:32 +0200

    Add screen breakpoints

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: { max: "425px" },
+      // => @media (max-width: 576px)
+
+      md: "960px",
+      // => @media (min-width: 960px) { ... }
+
+      lg: { min: "1440px", max: "1280px" },
+      // => @media (min-width: 1440px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [],
 };
